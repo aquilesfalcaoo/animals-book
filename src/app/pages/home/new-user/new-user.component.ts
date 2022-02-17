95ebc45147ad2de4c58c238a81c09dc2dc26f0ba
Aquiles Falcão <aquilesfalcaoo@gmail.com>
2022-02-16 21:43:11 -0300

    adicionado o método de formulário reativo

--- src/app/pages/home/new-user/new-user.component.ts
+++ src/app/pages/home/new-user/new-user.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { NewUserService } from 'src/app/core/services/new-user/new-user.service';
+import { NewUser } from 'src/app/shared/interface/new-user';
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.scss']
+  styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserComponent implements OnInit {
+  newUserForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private newUserService: NewUserService
+  ) {}
 
   ngOnInit(): void {
+    this.newUserForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
   }
 
+  cadastrar() {
+    const newUser = this.newUserForm.getRawValue() as NewUser;
+    console.log(newUser);
+  }
 }
