8f4a9fc17d5c8490d53d5126c39de2d6abd27c15
Aquiles Falcão <aquilesfalcaoo@gmail.com>
2022-02-18 12:32:47 -0300

    alterações nos elementos do site

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +12,23 @@ import { UserService } from '../../services/user/user.service';
 export class HeaderComponent {
   user$ = this.userService.retornarUser();
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {}
 
   logout() {
     this.userService.logout();
     this.router.navigate(['']);
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1000);
+    this.toastr.success('Desconectado!', 'Volte Sempre!', {
+      timeOut: 3000,
+      progressBar: true,
+    });
   }
 }
--- src/app/core/components/header/header.module.ts
+++ src/app/core/components/header/header.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxSpinnerModule],
   exports: [HeaderComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HeaderModule {}
