cba0ee07f15a8f69c5f28f86ab2a8c9516f5c9ad
Aquiles <aquilesfalcaoo@gmail.com>
2022-04-02 15:46:06 -0300

    instalado angular material

--- src/app/core/components/header/header.module.ts
+++ src/app/core/components/header/header.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { HeaderComponent } from './header.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule, NgxSpinnerModule],
+  imports: [CommonModule, RouterModule, NgxSpinnerModule, MatMenuModule],
   exports: [HeaderComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
