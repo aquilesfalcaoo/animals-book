91ec6972626b4dc1030da6af8dee927ec77afdef
Aquiles <aquilesfalcaoo@gmail.com>
2022-04-28 14:42:41 -0300

    feat: adicionado comentários nas fotos dos animais

--- src/app/pages/animals/animal-detail/comments/comments.component.ts
+++ src/app/pages/animals/animal-detail/comments/comments.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, switchMap, tap } from 'rxjs';
+import { CommentsService } from 'src/app/core/services/comments/comments.service';
+import { Comments } from 'src/app/shared/interface/comments/comments';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
+  @Input() id!: number;
+  comments$!: Observable<Comments>;
+  commentForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private commentsService: CommentsService,
+    private form: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.comments$ = this.commentsService.searchComment(this.id);
+    this.commentForm = this.form.group({
+      comment: [null, Validators.maxLength(300)],
+    });
   }
 
+  recording(): void {
+    const comment = this.commentForm.get('comment')?.value ?? '';
+    this.comments$ = this.commentsService.includeComment(this.id, comment).pipe(
+      switchMap(() => this.commentsService.searchComment(this.id)),
+      tap(() => {
+        this.commentForm.reset();
+      })
+    );
+  }
 }
